2b7b6051fdc8d58c07c6f33f0535746e97b6b4b7
CuseCose <dillemic000@gmail.com>
2020-11-13 19:44:19 -0500

    exports data too

--- Autonomous.py
+++ Autonomous.py
@@ -7,29 +7,25 @@ sys.path.append(r"c:\Users\liter\Desktop\engr100\Autonomous-py-new")
 sys.path.append(r"c:\Users\Mike\CLionProjects\Autonomous-py-new\Lib")
 sys.path.append(r"c:\Users\Mike\CLionProjects\Autonomous-py-new")
 
-
+import numpy
 import time
 from System.Drawing import Point
 from System.Windows import Forms
 import keyboard
-'''
-from graphics import *
-height=500
-width=500
-anchorpoint=Point(width/2,height/2)
-win = GraphWin("map_display_window", width, height)
-image=Image(anchorpoint, height, width)
 
-def draw():
-    globals()
-    rect=Rectangle(Point(0,0),Point(width,height))
-    rect.setFill("white")
-    rect.draw(win)
-    for p in pos:
-        c=Circle(Point(p[0],p[1]),2)
-        c.setFill("red")
-        #c.draw(win)
-'''
+p=[0,0]
+v=[0,0]
+vr=[0,0]
+a=[0,0]
+aer=[0,0]
+past=[[0,0]]
+
+def exportPast():
+    name = str(time.time())+".csv"
+    for i in past:
+        print(str(i[0])+" "+str(i[1]))
+    np.savetxt(name, past, delimiter=", ", fmt='% s')
+    p=[0,0]
 
 def takeoff():
     if cs.armed:
@@ -51,6 +47,7 @@ def land():
     else:
         print('Landing')
         Script.ChangeMode('Land')
+    exportPast()
 
 def disarm():
     if not cs.armed:
@@ -58,6 +55,7 @@ def disarm():
     else:
         print('Disarming motors')
         MAV.doARM(False)
+    exportPast()
 
 # Main loop
 
@@ -67,17 +65,14 @@ def stop():
     global running
     disarm()
     running=False
+    exportPast()
 
 keyboard.add_hotkey('esc', lambda: stop())
 
-p=[0,0]
-v=[0,0]
-a=[0,0]
-aer=[0,0]
 
 def main():
     globals()
-    global v,p,a,aer
+    global v,p,a,aer,vr
     for channel in range(1, 9):
         Script.SendRC(channel, 1500, True)
 
@@ -90,22 +85,30 @@ def main():
         dt=(time.time()-lasttime)
 
         if(method==1):#Method 1
-            fa = [cs.ax-aer[0], cs.ay-aer[1]] # Subtracts error from before takeoff
+            fa = [cs.ax-aer[0], cs.ay-aer[1]]  # Subtracts error from before takeoff
             if (cs.armed):
-                vn=[0,0];
                 for i in range(2):
-                    a[i]=(a[i]*.5+fa[i]*1.5)/2 # Slightly weighted to preventing rapid changes
-                    vn[i]=v[i]+dt*a[i] # Current calculated velocity
-                    p[i]=p[i]+dt*(v[i]+vn[i])/2 # Calculates average velocity over delta and uses that to compute pos
-                    v[i]=vn[i] # sets old speed as new speed
-                speed=sqrt(v[0]*v[0]+v[1]*v[1]) # Calculated velocity
-                measuredspeed=cs.airspeed # Measured Velocity
-                # If we can get speed and measured speed in the same units,
-                # we can then scale down the calculated speed so they have the same magnitude
+                    a[i] = (a[i]*.5+fa[i]*1.5)/2  # Slightly weighted to preventing rapid changes
+                    vn[i] = v[i]+dt*a[i]  # Current calculated velocity
 
-            else: # Runs when not flying, measures current error
-                aer[0]=(aer[0]+.1*cs.ax)/1.1
-                aer[1]=(aer[0]+.1*cs.ax)/1.1
+                speed = sqrt(vn[0]*vn[0]+vn[1]*vn[1])  # Calculated velocity
+                scale = 0 if speed == 0 else cs.airspeed/speed
+                vnr = [vn[0]*scale, vn[1]*scale]
+
+                for i in range(2):
+                    p[i] = p[i]+dt*(vr[i]+vnr[i])/2  # Calculates average velocity over delta and uses that to compute pos
+                    v[i] = vn[i] # sets old speed as new speed
+                    vr[i] = vnr[i] # sets old real speed as new speed
+
+                past.append([p[0],p[1]])
+
+            else:  # Runs when not flying, measures current error
+                for i in range(2):
+                    v[i]=0
+                    vr[i]=0
+                    a[i]=0
+                aer[0] = (aer[0]+.1*cs.ax)/1.1
+                aer[1] = (aer[0]+.1*cs.ax)/1.1
                 print("error = "+str(aer[0])+", "+str(aer[1]))
 
 
